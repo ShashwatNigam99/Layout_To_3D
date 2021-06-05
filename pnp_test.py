Fixed scaling and Rotation

pnp_test.py
@@ -15,11 +15,17 @@ K =  np.array([[293.33334351 ,           0.  ,        240.    ],
                [  0.         ,  0.           ,        1.      ]])
 
 P1_T = np.array([-7.082789897918701, -6.078360557556152, 5.098252296447754] )
-P1_R = np.array([90, 0, 90])
+P1_R = np.array([90.0, 0.0, 90.0])
+P1_R_matrix = np.array([[-0.0000,  1.0000, -0.0000],
+                       [-0.0000,  0.0000, -1.0000],
+                       [-1.0000, -0.0000,  0.0000]])
 
-P2_T = np.array([-7.082789897918701, -1.60836, 5.79825] )
+P2_T = np.array([-7.082789897918701, -2.38836, 5.098252296447754] )
 # P2_T = np.array([-7.082789897918701, -2.38836, 5.098252296447754] )
-P2_R = np.array([90, 0, 90])
+P2_R = np.array([90.0, 0.0, 82.0])
+P2_R_matrix = np.array([[0.1392, 0.9903, -0.0000],
+                       [0.0000, 0.0000, -1.0000],
+                       [-0.9903, 0.1392,  0.0000]])
 
 corners_list = np.array(corners_list)
 
@@ -50,21 +56,33 @@ T_change = -(P2_T - P1_T)
 #                         [T_change[2]],
 #                         [T_change[0]]])
 print(T_change)
-T_change = np.array([[T_change[1]],
-                        [-T_change[2]],
+T_change = np.array([[T_change[1]], # [0], [-T_change[0]]]);
+                     [-T_change[2]],
                         [-T_change[0]]])
+
 
 # T_change = np.array([[T_change[1]],
 #                         [T_change[2]],
 #                         [T_change[0]]])                       
 
-R_change = np.array([0.0,
-                     0.0, 
-                     0.0])
+# R_change = np.array([0.0,
+#                      0.0, 
+#                      0.0])
+
+# R_change = (P1_R - P2_R)
+# R_change = np.array([[R_change[0]], # [0], [-T_change[0]]]);
+#                      [R_change[1]],
+#                      [R_change[2]]])
+
+R_change_matrix = P1_R_matrix.T.dot(P2_R_matrix)
+R_change_vec, _ = cv2.Rodrigues(R_change_matrix)
+R_change_vec = np.array([[R_change_vec[1]], # [0], [-T_change[0]]]);
+                     [-R_change_vec[2]],
+                        [-R_change_vec[0]]])
 
 corners_array = np.array(corners_list)
 
-projected_points, _ = cv2.projectPoints( corners_list, R_change, T_change, K, dist_coeff)
+projected_points, _ = cv2.projectPoints( corners_list, R_change_vec, T_change, K, dist_coeff)
 # (_, rotation_vector, translation_vector) = cv2.solvePnP(corners_array, projected_points, K, dist_coeff, None, None, False, cv2.SOLVEPNP)
 
 (_, rotation_vector, translation_vector, inliers) = cv2.solvePnPRansac(corners_array, projected_points, K, dist_coeff)
@@ -75,7 +93,7 @@ print(rotation_vector)
 
 RGBimg_original = cv2.imread('./blendSample_1/blendSample/1.png')
 RGBimg_original = cv2.cvtColor(RGBimg_original, cv2.COLOR_BGR2RGB)
-RGBimg_slanted = cv2.imread('./blendSample_1/blendSample/2.png')
+RGBimg_slanted = cv2.imread('./blendSample_1/blendSample/4.png')
 RGBimg_slanted = cv2.cvtColor(RGBimg_slanted, cv2.COLOR_BGR2RGB)
 
 img1 = RGBimg_original

recon.py
@@ -14,7 +14,7 @@ import open3d as o3d
 LAYOUT_DIM = 512
 SHELVES = 4
 # SCALE= 0.015625
-SCALE= 0.038
+SCALE = 0.0390625
 K =  np.array([[293.33334351 ,           0.  ,        240.    ],
                [  0.         , 293.33334351  ,        135.    ],
                [  0.         ,  0.           ,        1.      ]])