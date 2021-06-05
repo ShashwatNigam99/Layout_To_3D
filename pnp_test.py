d0737b444b5ebc6e5cea52883f2d9899d79383c0
AnuragSahu <anuragatiit@gmail.com>
2021-06-05 23:22:56 +0530

    Automated images marking

--- pnp_test.py
+++ pnp_test.py
@@ -1,6 +1,7 @@
 # Testing
 
 from numpy.core.arrayprint import printoptions
+from scipy.spatial.transform.rotation import Rotation
 from recon import wrapper_func
 import numpy as np
 import cv2
@@ -13,20 +14,46 @@ K =  np.array([[293.33334351 ,           0.  ,        240.    ],
                [  0.         , 293.33334351  ,        135.    ],
                [  0.         ,  0.           ,        1.      ]])
 
+image_index = 6
+Positions = [np.array([-7.082789897918701, -2.38836, 5.098252296447754]),
+             np.array([-7.082789897918701, -1.60836, 5.79825]),
+             np.array([-7.082789897918701, -2.38836, 5.098252296447754]),
+             np.array([-7.082789897918701, -2.38836, 5.098252296447754]),
+             np.array([-5.02279, -5.95836, 5.098252296447754])]
+
+Rotations = [np.array([[-0.0000,  1.0000, -0.0000],
+                       [-0.0000,  0.0000, -1.0000],
+                       [-1.0000, -0.0000,  0.0000]]),
+             np.array([[-0.0000,  1.0000, -0.0000],
+                       [-0.0000,  0.0000, -1.0000],
+                       [-1.0000, -0.0000,  0.0000]]),
+             np.array([[0.1392, 0.9903, -0.0000],
+                       [-0.0000, 0.0000, -1.0000],
+                       [-0.9903, 0.1392,  0.0000]]),
+             np.array([[-0.0663,  0.9978,  0.0000],
+                       [-0.0000,  0.0000, -1.0000],
+                       [-0.9978, -0.0663,  0.0000]]),
+             np.array([[0.2470,  0.9690, -0.0000],
+                       [-0.0000, -0.0000, -1.0000],
+                       [-0.9690,  0.2470,  0.0000]])]
+
 P1_T = np.array([-7.082789897918701, -6.078360557556152, 5.098252296447754] )
 P1_R = np.array([90.0, 0.0, 90.0])
 P1_R_matrix = np.array([[-0.0000,  1.0000, -0.0000],
                        [-0.0000,  0.0000, -1.0000],
                        [-1.0000, -0.0000,  0.0000]])
 
-P2_T = np.array([-7.082789897918701, -1.60836, 5.79825] )
+P2_T = np.array([-5.02279, -5.95836, 5.098252296447754] )
 P2_R = np.array([90.0, 0.0, 82.0])
-P2_R_matrix = np.array([[-0.0000,  1.0000, -0.0000],
-                       [-0.0000,  0.0000, -1.0000],
-                       [-1.0000, -0.0000,  0.0000]])
+P2_R_matrix = np.array([[0.2470,  0.9690, -0.0000],
+                       [-0.0000, -0.0000, -1.0000],
+                       [-0.9690,  0.2470,  0.0000]])
 # P2_R_matrix = np.array([[0.1392, 0.9903, -0.0000],
 #                        [0.0000, 0.0000, -1.0000],
 #                        [-0.9903, 0.1392,  0.0000]])
+
+P2_T = Positions[image_index-2]
+P2_R_matrix = Rotations[image_index-2]
 
 corners_list = np.array(corners_list)
 
@@ -72,10 +99,9 @@ print("GT Rotations : ", R_change_vec)
 print("PnP Translations : ", translation_vector)
 print("PnP Rotations : ", rotation_vector)
 
-
 RGBimg_original = cv2.imread('./blendSample_1/blendSample/1.png')
 RGBimg_original = cv2.cvtColor(RGBimg_original, cv2.COLOR_BGR2RGB)
-RGBimg_slanted = cv2.imread('./blendSample_1/blendSample/3.png')
+RGBimg_slanted = cv2.imread('./blendSample_1/blendSample/'+str(image_index)+'.png')
 RGBimg_slanted = cv2.cvtColor(RGBimg_slanted, cv2.COLOR_BGR2RGB)
 
 img1 = RGBimg_original
@@ -84,15 +110,18 @@ img2 = RGBimg_slanted
 
 for i in projected_points:
     # print(int(i[0][0]),int(i[0][1]))
-    img2 = cv2.circle(img2, (int(i[0][0]),int(i[0][1])), radius=1, color=(0, 0, 255), thickness=1)
+    # img2 = cv2.circle(img2, (int(i[0][0]),int(i[0][1])), radius=1, color=(0, 0, 255), thickness=2)
+    cv2.drawMarker(img2, (int(i[0][0]),int(i[0][1])),(0,0,255), markerType=cv2.MARKER_STAR, 
+    markerSize=4, thickness=2, line_type=cv2.LINE_AA)
 
 
 for i in projected_points_PnP:
     # print(int(i[0][0]),int(i[0][1]))
-    img2 = cv2.circle(img2, (int(i[0][0]),int(i[0][1])), radius=1, color=(0, 255, 255), thickness=1)
+    # img2 = cv2.circle(img2, (int(i[0][0]),int(i[0][1])), radius=1, color=(255, 255, 0), thickness=2)
+    cv2.drawMarker(img2, (int(i[0][0]),int(i[0][1])),(255, 255, 0), markerType=cv2.MARKER_SQUARE , 
+    markerSize=2, thickness=1, line_type=cv2.LINE_AA)
 
-# for i,j in zip(projected_points_PnP, projected_points):
-    # print(i,j)
+cv2.imwrite(str(image_index)+"_with_GT_PNP.png", cv2.cvtColor(img2, cv2.COLOR_BGR2RGB))
 
 plt.imshow(img2)
 plt.show()
