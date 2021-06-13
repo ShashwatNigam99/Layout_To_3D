b3369aaff31ab295aab42ea66f012511bd7155b9
Avinash Prabhu <avinashprabhu2001@gmail.com>
2021-06-14 00:23:31 +0400

    Added code to get ground truth 3D points from blender

---
+++ Groundtruth_3Dpoints_blender.py
@@ -0,0 +1,104 @@
+import bpy
+from bpy import context
+import numpy as np
+from mathutils import Matrix
+
+def get_3x4_RT_matrix_from_blender(cam):
+    # bcam stands for blender camera
+    R_bcam2cv = Matrix(
+        ((1, 0,  0),
+         (0, -1, 0),
+         (0, 0, -1)))
+
+    location, rotation = cam.matrix_world.decompose()[0:2]
+    R_world2bcam = rotation.to_matrix().transposed()
+   
+    T_world2bcam = -1*R_world2bcam @ location
+
+    R_world2cv = R_bcam2cv@R_world2bcam
+    T_world2cv = R_bcam2cv@T_world2bcam
+
+    # put into 3x4 matrix
+    RT = Matrix((
+        R_world2cv[0][:] + (T_world2cv[0],),
+        R_world2cv[1][:] + (T_world2cv[1],),
+        R_world2cv[2][:] + (T_world2cv[2],)
+         ))
+    return RT
+
+def get_calibration_matrix_K_from_blender(mode='simple'): # To verify the K matrix
+
+    scene = bpy.context.scene
+
+    scale = scene.render.resolution_percentage / 100
+    width = scene.render.resolution_x * scale # px
+    height = scene.render.resolution_y * scale # px
+
+    camdata = scene.camera.data
+
+    if mode == 'simple':
+
+        aspect_ratio = width / height
+        K = np.zeros((3,3), dtype=np.float32)
+        K[0][0] = width / 2 / np.tan(camdata.angle / 2)
+        K[1][1] = height / 2. / np.tan(camdata.angle / 2) * aspect_ratio
+        K[0][2] = width / 2.
+        K[1][2] = height / 2.
+        K[2][2] = 1.
+        K.transpose()
+    
+    if mode == 'complete':
+
+        focal = camdata.lens # mm
+        sensor_width = camdata.sensor_width # mm
+        sensor_height = camdata.sensor_height # mm
+        pixel_aspect_ratio = scene.render.pixel_aspect_x / scene.render.pixel_aspect_y
+
+        if (camdata.sensor_fit == 'VERTICAL'):
+            # the sensor height is fixed (sensor fit is horizontal), 
+            # the sensor width is effectively changed with the pixel aspect ratio
+            s_u = width / sensor_width / pixel_aspect_ratio 
+            s_v = height / sensor_height
+        else: # 'HORIZONTAL' and 'AUTO'
+            # the sensor width is fixed (sensor fit is horizontal), 
+            # the sensor height is effectively changed with the pixel aspect ratio
+            pixel_aspect_ratio = scene.render.pixel_aspect_x / scene.render.pixel_aspect_y
+            s_u = width / sensor_width
+            s_v = height * pixel_aspect_ratio / sensor_height
+
+        # parameters of intrinsic calibration matrix K
+        alpha_u = focal * s_u
+        alpha_v = focal * s_v
+        u_0 = width / 2
+        v_0 = height / 2
+        skew = 0 # only use rectangular pixels
+
+        K = np.array([
+            [alpha_u,    skew, u_0],
+            [      0, alpha_v, v_0],
+            [      0,       0,   1]
+        ], dtype=np.float32)
+    
+    return K
+
+cam =  bpy.data.objects["camera"]
+RT_matrix_camera = get_3x4_RT_matrix_from_blender(cam)
+print(RT_matrix_camera)
+obj_points = []
+
+for obj in bpy.context.scene.objects: 
+    if obj.name[0:3] == "Box" and (obj.name[3] != "A" and obj.name[3] != "F") : # Some boxes don't have exactly 8 vertices(mesh problem)
+
+        obj = bpy.data.objects[obj.name]
+        list_temp = []
+        for v in obj.data.vertices:
+            pointie = np.array(RT_matrix_camera @ obj.matrix_world @ v.co)
+            list_temp.append(pointie)
+        
+        print(len(list_temp))
+        list_temp = np.array(list_temp)
+        obj_points.append(list_temp)
+
+print(len(obj_points))
+obj_points = np.array(obj_points)
+np.save("3Dpoints1.npy",obj_points)
